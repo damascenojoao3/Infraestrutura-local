ea44f84e1819188a19df021cbd8a92cfda566979
João Pedro Lima Damasceno <damascenolima19@gmail.com>
2025-08-07 15:09:02 -0300

    Add files via upload

---
+++ monitorar_site.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# URL do site
+URL="http://localhost"
+
+# Caminho para o log
+LOG="/var/log/monitoramento.log"
+
+# Webhook
+WEBHOOK_URL="https://discord.com/api/webhooks/1402699611478560830/LjB111tSW46VfApobbBR-ehVPLBw30GN98XhtHb1SozTxFU27C-S-sY8gCdKSUHi5Qyb"
+
+# Data e hora atual
+DATA=$(/bin/date '+%Y-%m-%d %H:%M:%S')
+
+# Verifica se o site responde (200 = OK)
+HTTP_CODE=$(/usr/bin/curl -s -o /dev/null -w "%{http_code}" $URL)
+
+# Se o código HTTP for diferente de 200, vai considerar como erro
+if [ "$HTTP_CODE" -ne 200 ]; then
+    MENSAGEM="$DATA - ERRO: Site indisponível! Código HTTP: $HTTP_CODE"
+    
+    # Grava no log
+    echo "$MENSAGEM" >> $LOG
+
+    # Envia alerta para o webhook
+    curl -H "Content-Type: application/json" \
+         -X POST \
+         -d "{\"content\": \"$MENSAGEM\"}" \
+         $WEBHOOK_URL
+else
+    echo "$DATA - OK: Site funcionando normalmente. Código HTTP: $HTTP_CODE" >> $LOG
+fi
